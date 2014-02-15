f4b521372cc6d392772dff71a3c6e14056bff355
NaokiHanakawa <naoki.hanakawa@gmail.com>
2014-02-16 02:34:21 +0900

    パスを絶対パスに変更

--- os.sh
+++ os.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 #uname > /dev/null
-
 if [ `uname` = "Darwin" ] 
 then
-    ./ipaddr_m.sh
+  ~/.zsh.d/ipaddr_m.sh
 elif [ `uname` = "Linux" ]
  then
-    ./ipaddr_l.sh
+  ~/.zsh.d/ipaddr_l.sh
 fi
